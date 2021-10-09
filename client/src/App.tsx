af463acc2a0eec5e76580038a1c2e60e46b32be3
vladrassokhyn <perishvlad@gmail.com>
2021-10-09 15:51:17 +0300

    feat: access page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router';
+import { Analitic } from './pages/Analitic/Analitics';
 import { CmMenu } from './pages/CM/CmMenu';
 import { Main } from './pages/Main/Main';
 
@@ -8,6 +9,7 @@ export const App = (): JSX.Element => {
     <>
       <Route exact path={'/'} render={() => <Main />} />
       <Route exact path={'/cm'} render={() => <CmMenu />} />
+      <Route exact path={'/analytics'} render={() => <Analitic />} />
     </>
   );
 };
---
+++ client/src/pages/Analitic/Analitics.tsx
@@ -0,0 +1,3 @@
+export const Analitic = (): JSX.Element => {
+  return <h1>Access denied. You are not admin member</h1>;
+};
