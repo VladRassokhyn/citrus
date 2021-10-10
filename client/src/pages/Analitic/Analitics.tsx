1cfe2bc0df87cf10c080a9ea19f8159c8676f337
vladrassokhyn <perishvlad@gmail.com>
2021-10-10 21:50:29 +0300

    update

--- client/src/pages/Analitic/Analitics.tsx
+++ client/src/pages/Analitic/Analitics.tsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from 'react';
 import { Preloader } from '../../Components/Preloader';
 
 export const Analitic = (): JSX.Element => {
-  return <Preloader />;
+  const [show, setSHow] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSHow(true);
+    }, 2000);
+  }, []);
+
+  if (show) {
+    return <h2>Самозванец</h2>;
+  } else {
+    return <Preloader />;
+  }
 };
