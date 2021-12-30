e16990f084ae3b3719f8c1772efb7719dd8530a4
vladrassokhyn <perishvlad@gmail.com>
2021-12-30 16:58:28 +0200

    add raiting client

--- client/src/pages/Analitics/Raiting/Raiting.tsx
+++ client/src/pages/Analitics/Raiting/Raiting.tsx
@@ -7,6 +7,8 @@ import { User } from '@lib/slices/users';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import Selector from 'react-select';
+import { selectorOptions } from './selectorOptions';
 
 type Props = {
   authUser: User;
@@ -19,9 +21,14 @@ const Row = styled.div`
   flex-direction: row;
 `;
 
-const H5 = styled.h1`
+const H2 = styled.h1`
   font-size: 10pt;
-  width: 300px;
+  width: 100px;
+`;
+
+const H1 = styled.h1`
+  font-size: 10pt;
+  width: 400px;
 `;
 
 export const Raiting = (props: Props): JSX.Element => {
@@ -30,7 +37,7 @@ export const Raiting = (props: Props): JSX.Element => {
   const { month, year } = useTypedSelector(salesSelectors.monthAndYear);
   const sales = useTypedSelector(raitingSelectors.sales);
   const status = useTypedSelector(raitingSelectors.status);
-  console.log('render');
+
   useEffect(() => {
     dispatch(raitingActions.getSales({ month, year }));
   }, []);
@@ -43,13 +50,26 @@ export const Raiting = (props: Props): JSX.Element => {
     return <Preloader />;
   }
 
+  const onChange = (e: any) => {
+    setCategory(e.value);
+  };
+
   return (
     <Wrapper>
-      {sales?.map((sale: any) => {
+      <Row>
+        <h4>service: </h4>
+        <Selector
+          options={selectorOptions}
+          onChange={onChange}
+          defaultValue={{ label: 'ЦМ', value: SalesIndexes.CM }}
+        />
+      </Row>
+      {sales?.map((sale: any, i: number) => {
         return (
           <Row key={sale[0]}>
-            <H5>{sale[0]}</H5>
-            <H5>{sale[category]}</H5>
+            <h5>{i + 1}. </h5>
+            <H1>{sale[0]}</H1>
+            <H2>{sale[category]}</H2>
           </Row>
         );
       })}
---
+++ client/src/pages/Analitics/Raiting/selectorOptions.ts
@@ -0,0 +1,24 @@
+import { SalesIndexes } from '@lib/slices/sales';
+
+export const selectorOptions = [
+  {
+    label: 'ТО',
+    value: SalesIndexes.TO,
+  },
+  {
+    label: 'Устройства',
+    value: SalesIndexes.DEVICES,
+  },
+  {
+    label: 'ЦМ',
+    value: SalesIndexes.CM,
+  },
+  {
+    label: 'ЦЗ',
+    value: SalesIndexes.CZ,
+  },
+  {
+    label: 'ЦЗ',
+    value: SalesIndexes.CA,
+  },
+];
